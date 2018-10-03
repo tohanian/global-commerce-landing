783e133011b8cdafcc5d5f70ed965d6378ebefd5
tohanian <tadehohanian@gmail.com>
2018-10-03 07:46:21 -0400

    Adds logic for carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 import '../styles/styles.scss';
+
+// Event handlers
+
+const carouselSelectors = document
+  .querySelector('.carousel-selectors')
+  .getElementsByTagName('li');
+for (let i = 0; i < carouselSelectors.length; i++) {
+  carouselSelectors[i].addEventListener('click', selectItem(i));
+}
+
+function selectItem(selectedItem) {
+  return function() {
+    const images = document
+      .querySelector('.carousel-image')
+      .getElementsByTagName('li');
+    for (let i = 0; i < images.length; i++) {
+      if (i !== selectedItem) {
+        images[i].style.display = 'none';
+      } else {
+        images[i].style.display = 'block';
+      }
+    }
+  };
+}
